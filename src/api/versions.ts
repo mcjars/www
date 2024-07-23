869230e9d49b9758037609fdbfaf214cffd16b3e
0x7d8 <rjansengd@gmail.com>
2024-07-23 02:32:33 +0200

    slight bandwidth optimizations

--- src/api/versions.ts
+++ src/api/versions.ts
@@ -1,29 +1,21 @@
 import axios from "axios"
 
 type MinecraftVersion = {
-	type?: 'RELEASE' | 'SNAPSHOT'
-	supported?: boolean
-	created?: string
-	java?: number
+	type: 'RELEASE' | 'SNAPSHOT'
+	supported: boolean
+	created: string
+	java: number
 	builds: number
 	latest: {
-		id: number
-		type: string
 		projectVersionId: string | null
 		versionId: string | null
-		buildNumber: number
-		created: string
-		jarUrl: string | null
-		jarSize: number | null
-		zipUrl: string | null
-		zipSize: number | null
 	}
 }
 
 export default async function apiGetVersions(type: string): Promise<MinecraftVersion[]> {
 	const { data } = await axios.get<{
 		builds: Record<string, MinecraftVersion>
-	}>(`https://versions.mcjars.app/api/v2/builds/${type.toUpperCase()}`)
+	}>(`https://versions.mcjars.app/api/v2/builds/${type.toUpperCase()}?fields=projectVersionId,versionId`)
 
 	return Object.values(data.builds).reverse()
 }
