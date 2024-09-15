5406be4cabe426f0f568c23dfb8fe53a6d32ef36
Anthony M. <anthony19990maalouf@gmail.com>
2024-09-15 23:26:07 +0300

    Refactor Waterfall warning component to include notice about using Velocity as a better alternative to Waterfall.

--- src/components/waterfall-warning.tsx
+++ src/components/waterfall-warning.tsx
@@ -18,6 +18,12 @@ export function WaterfallWarning({ open, onGoToVelocity, onGoToBungeeCord }: Wat
 					<DialogDescription>
                         Waterfall is <p className={'inline font-bold'}>DEPRECATED</p> and no longer supported,
                         instead you should consider an alternative like <p className={'inline hover:underline cursor-pointer text-blue-500'} onClick={onGoToBungeeCord}>BungeeCord</p> or <p className={'inline hover:underline cursor-pointer text-blue-500'} onClick={onGoToVelocity}>Velocity</p>.
+
+						<br />
+						<br />
+
+						<p className={'inline font-bold'}>NOTICE</p>: <p className={'inline hover:underline cursor-pointer text-blue-500'} onClick={onGoToVelocity}>Velocity</p> is a much better alternative to Waterfall than <p className={'inline hover:underline cursor-pointer text-blue-500'} onClick={onGoToBungeeCord}>BungeeCord</p>,
+						due to its security and performance improvements.
 					</DialogDescription>
 				</DialogHeader>
 			</DialogContent>
