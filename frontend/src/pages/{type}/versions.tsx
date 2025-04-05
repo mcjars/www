d6458ea391480dc9fd23c05a6c4f702e7d15e22e
Anthony01M <anthony19990maalouf@gmail.com>
2025-04-05 14:18:27 +0000

    feat: tooltip (mobile)

--- frontend/src/pages/{type}/versions.tsx
+++ frontend/src/pages/{type}/versions.tsx
@@ -5,6 +5,7 @@ import { Alert, AlertDescription } from "@/components/ui/alert"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"
 import { Drawer, DrawerContent, DrawerHeader, DrawerTitle } from "@/components/ui/drawer"
 import { Input } from "@/components/ui/input"
@@ -26,11 +27,11 @@ export default function PageTypeVersions() {
 
 	const mobile = useIsMobile(1280)
 
-	const [ versionType, setVersionType ] = useQueryParam('type', StringParam)
-	const [ search, setSearch ] = useQueryParam('search', StringParam)
-	const [ browse, setBrowse ] = useQueryParam('browse', StringParam)
-	const [ displayMode, setDisplayMode ] = useQueryParam('display', StringParam)
-	const [ installScript, setInstallScript ] = useLocalStorage<'bash' | 'mcvcli'>('install-script', 'bash')
+	const [versionType, setVersionType] = useQueryParam('type', StringParam)
+	const [search, setSearch] = useQueryParam('search', StringParam)
+	const [browse, setBrowse] = useQueryParam('browse', StringParam)
+	const [displayMode, setDisplayMode] = useQueryParam('display', StringParam)
+	const [installScript, setInstallScript] = useLocalStorage<'bash' | 'mcvcli'>('install-script', 'bash')
 
 	const { data: types } = useSWR(
 		['types'],
@@ -55,12 +56,12 @@ export default function PageTypeVersions() {
 
 	const expectedBuildCount = useMemo(
 		() => !browse ? 0 : versions?.find((version) => version.latest.versionId === browse)?.builds ?? 0,
-		[ versions, browse ]
+		[versions, browse]
 	)
 
 	const typeData = useMemo(
 		() => Object.values(types ?? {}).flat()?.find((t) => t.identifier === type),
-		[ types, type ]
+		[types, type]
 	)
 
 	return (
@@ -160,7 +161,14 @@ export default function PageTypeVersions() {
 													{version.type}
 												</Badge>
 												{version.type === 'SNAPSHOT' && (
-													<TriangleAlertIcon size={16} className={'ml-2 text-red-500 md:hidden'} />
+													<Tooltip>
+														<TooltipTrigger>
+															<TriangleAlertIcon size={16} className={'ml-2 text-red-500 md:hidden'} />
+														</TooltipTrigger>
+														<TooltipContent>
+															<p>SNAPSHOT</p>
+														</TooltipContent>
+													</Tooltip>
 												)}
 											</h1>
 											<p className={'text-sm text-gray-500'}>
